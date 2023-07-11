930c49afdf3b603640220a660f9b63f50a194908
davidfer78 <davidgomezmunera@gmail.com>
2023-07-11 12:26:50 -0500

    add de imagenes al carrusel- revisar el nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+            <div className="relative flex h-18 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -37,7 +37,7 @@ export default function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-center sm:justify-between">
+              <div className="flex flex-1 items-center justify-center sm:items-center sm:justify-between md:flex-col lg:flex-row">
                 <div className="flex flex-shrink-0 items-center">
                   <Image
                     src={logo_cn}
