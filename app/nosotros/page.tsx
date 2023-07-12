67565758d2c731fcfa6ce24cd0fe548e120cead0
sebastian009w <ia3266505@gmail.com>
2023-07-12 09:31:18 -0500

    Creacion de la pagina nosotros (#20)

--- app/nosotros/page.tsx
+++ app/nosotros/page.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import nosotros from '@/public/nosotros.png'
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { Typography } from "@mui/material";
+
 export default function PageAbout() {
-    return <div>
-        <h1>nosotros</h1>
-    </div>
+    return <Grid container spacing={2}>
+        <Grid xs={6}>
+        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Nosotros</h2>
+        <Typography mt={2}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae magnam quod autem, commodi, exercitationem voluptatem sapiente in, maxime aperiam itaque consequuntur dolorem minus ratione cum voluptate cumque aliquid perferendis nulla!</Typography>
+        </Grid>
+        <Grid xs={6}>
+            <Image src={nosotros} alt="Quienes somos nosotros costanet"/>
+        </Grid>
+    </Grid> 
 }
--- components/Carrusel.tsx
+++ components/Carrusel.tsx
@@ -2,20 +2,20 @@
 
 import { Carousel } from "flowbite-react";
 import logoCostanet from "@/public/logo2.png";
-import preimgcrr from "@/public/pre1.png";
-import segimgcrr from "@/public/pre2.png";
-import terimgcrr from "@/public/pre3.png";
-import cuaimgcrr from "@/public/pre4.png";
+// import preimgcrr from "@/public/pre1.png";
+// import segimgcrr from "@/public/pre2.png";
+// import terimgcrr from "@/public/pre3.png";
+// import cuaimgcrr from "@/public/pre4.png";
 import Image from "next/image";
 
 export default function SlidingInterval() {
   return (
     <Carousel slideInterval={5000} className="h-screen rounded-none">
       <Image alt="costanet" src={logoCostanet} className="rounded-none" />
-      <Image alt="costanet" src={preimgcrr} className="rounded-none"/>
-      <Image alt="costanet" src={segimgcrr} className="rounded-none" />
-      <Image alt="costanet" src={terimgcrr} className="rounded-none"/>
-      <Image alt="costanet" src={cuaimgcrr} className="rounded-none"/>    
+      <Image alt="costanet" src={logoCostanet} className="rounded-none"/>
+      <Image alt="costanet" src={logoCostanet} className="rounded-none" />
+      <Image alt="costanet" src={logoCostanet} className="rounded-none"/>
+      <Image alt="costanet" src={logoCostanet} className="rounded-none"/>    
     </Carousel>
   );
 }
