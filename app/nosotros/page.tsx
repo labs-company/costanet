cedd5b2463665b4a9b044acafcb58726266a4daf
JS9139 <johancs@gmail.com>
2023-07-13 17:10:14 -0500

    se modifica la seccion nosotros (#34)

--- app/nosotros/page.tsx
+++ app/nosotros/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import nosotros from '@/public/nosotros.png'
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { Typography } from "@mui/material";
 
@@ -8,10 +7,6 @@ export default function PageAbout() {
         <Grid xs={6}>
         <h2 className="text-3xl font-bold leading-7  sm:truncate sm:text-3xl sm:tracking-tight text-letter">NOSOTROS</h2>
         <Typography mt={3}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae magnam quod autem, commodi, exercitationem voluptatem sapiente in, maxime aperiam itaque consequuntur dolorem minus ratione cum voluptate cumque aliquid perferendis nulla!</Typography>
-        </Grid>
-        <Grid xs={6}>
-            <Image src={nosotros} alt="Quienes somos nosotros costanet"/>
-            
         </Grid>
     </Grid> 
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Carrusel from "@/components/Carrusel";
 import Container from "@mui/material/Container";
 import PageAbout from "./nosotros/page";
-
+// import second from '@/public/backgroundUs.png'
 export default function Home() {
   return (
     <>
@@ -9,7 +9,7 @@ export default function Home() {
         <section>
           <Carrusel />
         </section>
-        <section className="bg-section">
+        <section className="bg-section bg-[url('../public/backgroundUs.png')] w-full bg-no-repeat bg-cover h-screen">
           <Container className="pt-24">
             <PageAbout />
           </Container>
