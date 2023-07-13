ddf308da11d10765416e6321a1eab7a0f50cddd3
sebastian009w <ia3266505@gmail.com>
2023-07-13 10:45:40 -0500

    estilo css para el test (#24)

--- app/test-de-velocidad/page.tsx
+++ app/test-de-velocidad/page.tsx
@@ -4,7 +4,11 @@ export default function TestDeVelocidad() {
   return (
     <Container className="bg-navbar">
       <div className="loader-widget">
-        <span className="loader"></span>
+        <span className="loader hiden"></span>
+        <div className="loader-content">
+            <div className="content"></div>
+            <button>EMPEZAR</button>
+        </div>
       </div>
     </Container>
   );
