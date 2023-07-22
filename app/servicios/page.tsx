b5749e3694ae08a972a507b95f04873d7e9b7941
JS9139 <johancs@gmail.com>
2023-07-22 08:52:26 -0500

    se hace otra modificación de la seccion servicios tarea (#56)

--- app/servicios/page.tsx
+++ app/servicios/page.tsx
@@ -7,23 +7,44 @@ import wifiCostanet from "@/public/sectionTres/wifi-costanet.png";
 export default function PageServices() {
   return (
     <>
-      <section className="bg-letter">
-        <Container className="flex flex-col justify-center">
-          <div className="pt-5">
-            <p className="text-4xl xl:text-2xl text-white font-medium ml-1">
-              Hacemos clientes felices
-            </p>
-          </div>
-          <Grid container spacing={2}>
-            <Grid xs={6}>
-              <Image src={clientHappy} alt="Hacemos clientes felices" />
+      <section className="bg-letter h-screen flex flex-col justify-stretch">
+        <Container className="flex flex-col justify-center h-screen">
+          <Grid
+            container
+            spacing={2}
+            sx={{
+              margin: "0px",
+            }}
+          >
+            <Grid
+              xs={5}
+              sx={{
+                padding: "0px",
+                width: "auto",
+              }}
+            >
+              <Image
+                src={clientHappy}
+                alt="Hacemos clientes felices"
+                className="w-full flex items-end"
+              />
             </Grid>
-            <Grid xs={6}>
-              <figure className="flex flex-col items-center justify-center">
-                <Image src={wifiCostanet} alt="Servicio de wifi" width={500} />
-                <figcaption className="text-3xl text-white">
-                  Servicio wifi
-                </figcaption>
+            <Grid
+              xs={7}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+              }}
+            >
+              <div className="pt-5 p-0 flex content-center">
+                <p className="text-4xl xl:text-6xl text-center text-white font-medium ml-1">
+                  Con nuestro servicio WIFI hacemos felices a nuestros clientes
+                </p>
+              </div>
+
+              <figure className="flex flex-col items-center justify-end">
+                <Image src={wifiCostanet} alt="Servicio de wifi" width={200} />
               </figure>
             </Grid>
           </Grid>
