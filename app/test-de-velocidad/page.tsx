c214f1b8a74abf484cbafd7b302cda4b58ab4d73
sebastian009w <ia3266505@gmail.com>
2023-07-25 14:37:16 -0500

    fix: Contacto responsive

--- app/test-de-velocidad/page.tsx
+++ app/test-de-velocidad/page.tsx
@@ -6,7 +6,9 @@ export default function TestDeVelocidad() {
     <section className="flex flex-col justify-center items-center p-8">
       <div className="bg-white p-5">
         <div className="flex flex-col justify-center items-center text-justify gap-2">
-          <h2 className="text-5xl font-bold text-letter">TEST DE VELOCIDAD</h2>
+          <h2 className="sm:text-5xl font-bold text-letter text-lg">
+            TEST DE VELOCIDAD
+          </h2>
           <p>Examina la velocidad de tu red</p>
           <p>
             Realiza la medición de tus servicios de internet, desde tu celular,
