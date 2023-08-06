59efb1cfc3bff80b445c0886d14bec13c9d54b00
Sebastian <johancs.mm@gmail.com>
2023-08-05 22:02:10 -0500

    fix: title pqrs

--- app/pqrs/page.tsx
+++ app/pqrs/page.tsx
@@ -4,6 +4,7 @@ import { Container } from "@mui/material";
 export default function PagePqrs() {
   return (
     <section>
+      <h2 className="text-2xl">PQRS</h2>
       <Container>
         <TabsPqrs />
       </Container>
