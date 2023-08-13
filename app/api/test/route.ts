11d212b88edf48275145352904693f91d734b9ec
Sebastian <johancs.mm@gmail.com>
2023-08-12 22:58:22 -0500

    chore: informacion del wifi

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,15 +2,9 @@ import { NextResponse } from 'next/server';
 import speedtest from '@/utils/speedtest';
 
 export async function GET() {
-    const result = await speedtest()
-    console.log(result);
-    return NextResponse.json({
-        message: "Hello APi"
-    })
-}
+    const results = await speedtest()
 
-export function POST() {
     return NextResponse.json({
-        date: "datos"
+        results
     })
 }
