e91164ae7885b715b62814cff810ef7d8ef1aa5a
Sebastian <johancs.mm@gmail.com>
2023-08-14 13:44:42 -0500

    fix: navegacion del lado del servidor

---
+++ components/DisclosureNavbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { navigation, classNames } from "@/utils/costanet_routes";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+
+export default function DisclosureNavbar() {
+  const [currentNavItem, setCurrentNavItem] = useState(
+    navigation.find((item) => item.current)
+  );
+  return (
+    <Disclosure as="div" className="absolute sm:hidden w-full">
+      {({ open }) => (
+        <>
+          <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-letter hover:bg-letter hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span className="sr-only">Open main menu</span>
+            {open ? (
+              <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+            ) : (
+              <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+            )}
+          </Disclosure.Button>
+          <Disclosure.Panel className="sm:hidden bg-navbar p-4">
+            <div className="pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  onClick={() => setCurrentNavItem(item)}
+                  className={classNames(
+                    item.name === currentNavItem?.name
+                      ? "bg-letter text-white"
+                      : "text-letter hover:bg-letter hover:text-white",
+                    "block rounded-md py-2 text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
