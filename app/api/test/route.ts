88a063a2d407cbd1bdf82420c0c040f6222621dd
Sebastian <johancs.mm@gmail.com>
2023-08-13 19:25:10 -0500

    fix: correcion en peticion mbps

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,51 +1,27 @@
 import { NextResponse } from 'next/server';
 // import speedtest from '@/utils/speedtest';
 // @ts-ignore
-// import FastSpeedTest from 'fast-speedtest-api';
-import https from "node:https";
+import FastSpeedTest from 'fast-speedtest-api';
 
 export async function GET() {
     // const results = await speedtest()
 
-    // const speedtest = new FastSpeedTest({
-    //     token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
-    //     unit: FastSpeedTest.UNITS.Mbps, // default: Bps
-    //     proxy: 'http://optional:auth@my-proxy:123' // def
+    const speedtest = new FastSpeedTest({
+        token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
+        verbose: false, // default: false
+        timeout: 10000, // default: 5000
+        https: true, // default: true
+        urlCount: 5, // default: 5
+        bufferSize: 8, // default: 8
+        unit: FastSpeedTest.UNITS.Mbps, // default: Bps
+        proxy: 'http://optional:auth@my-proxy:123' // default: undefined
 
-    // })
-
-
-
-    const startTime = Date.now();
-
-    const results = [];
-    https.get('https://fast.com/', (res) => {
-
-        res.on('data', (chunk) => {
-            data.push(chunk);
-        });
-
-        res.on('end', () => {
-            const endTime = Date.now();
-            const duration = (endTime - startTime) / 1000; // en segundos
-
-            const totalBytes = Buffer.concat(data).length;
-            const speed = (totalBytes / duration) / (1024 * 1024); // en MB/s
-
-            data.push({
-                totalBytes,
-                speed,
-            })
-            console.log(`Velocidad aproximada: ${speed.toFixed(2)} MB/s`);
-        });
-    }).on('error', (err) => {
-        console.error('Error:', err);
-        return;
-    });
+    })
 
 
+    const results = await speedtest.getSpeed()
 
-    // const results = await speedtest.getSpeed()
+
 
     return NextResponse.json({
         results
