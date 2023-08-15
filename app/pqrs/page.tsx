e01981350f8e55d10434787839700860bb2a2426
Sebastian <johancs.mm@gmail.com>
2023-08-15 14:31:46 -0500

    style: Something

--- app/pqrs/page.tsx
+++ app/pqrs/page.tsx
@@ -3,6 +3,7 @@ import TabsPqrs from "@/components/TabsPqrs";
 export default function PagePqrs() {
   return (
     <section className="flex justify-center items-center">
+      <div>En desarrollo...</div>
       <TabsPqrs />
     </section>
   );
