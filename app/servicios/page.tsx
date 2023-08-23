f559dfa1998bef18ff95049c328c8243a6881c2d
JS9139 <johancs@gmail.com>
2023-08-23 16:40:11 -0500

    fix: Cambio de background

--- app/servicios/page.tsx
+++ app/servicios/page.tsx
@@ -1,9 +1,19 @@
 import { Container } from "@mui/material";
 import ServiceClientComponet from "@/components/ServiceClientComponet";
+import backgroundus from "@/public/bg-example.jpg";
+
 export default function PageServices() {
   return (
     <>
-      <section className="bg-letter flex flex-col justify-end" id="servicios">
+      <section
+        className="flex flex-col justify-end"
+        id="servicios"
+        style={{
+          backgroundImage: `url(${backgroundus.src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
         <Container className="flex flex-col-reverse justify-center">
           <ServiceClientComponet />
         </Container>
