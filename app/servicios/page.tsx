fb119b26316ef3e43127906aa64b9c566e26c940
Sebastian <johancs.mm@gmail.com>
2023-08-27 13:53:24 -0500

    fix: se elimina el background

--- app/servicios/page.tsx
+++ app/servicios/page.tsx
@@ -1,19 +1,12 @@
 import { Container } from "@mui/material";
 import ServiceClientComponet from "@/components/ServiceClientComponet";
-import backgroundus from "@/public/bg-example.jpg";
 
 export default function PageServices() {
   return (
     <>
       <section
-        className="flex flex-col justify-end"
-        id="servicios"
-        style={{
-          backgroundImage: `url(${backgroundus.src})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      >
+        className="flex flex-col justify-end bg-letter"
+        id="servicios">
         <Container className="flex flex-col-reverse justify-center">
           <ServiceClientComponet />
         </Container>
